a46b14ee1313782a13be739ea104296ffe09e539
Jack Foster <jackfoster246@gmail.com>
2022-12-15 22:52:43 -0500

    Change port number to 8080 for URL

--- client/src/components/purchaseIngredient.js
+++ client/src/components/purchaseIngredient.js
@@ -22,7 +22,7 @@ export default function PurchaseIngredient() {
    // When a post request is sent to the create url, we'll add a new record to the database.
    const newPerson = { ...form };
  
-   await fetch("http://localhost:5000/ingredient/add", {
+   await fetch("http://localhost:8080/ingredient/add", {
      method: "POST",
      headers: {
        "Content-Type": "application/json",
